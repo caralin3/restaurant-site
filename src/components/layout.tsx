eb27b0713724b4e906a15d38539026fc65c8df3c
csteinberg <csteinberg@geekhive.com>
2019-01-24 11:48:28 -0500

    Fixed helmet title

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -15,7 +15,7 @@ interface LayoutProps {
 export const Layout: React.SFC<LayoutProps> = (props) => (
   <div className={styles.layout}>
     <Helmet
-      title={`${props.pageTitle} | ${props.siteTitle}`}
+      title={!!props.pageTitle ? `${ props.pageTitle} | ${props.siteTitle}` : props.siteTitle}
       link={[{ rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }]}
     />
     <Navigation title={props.siteTitle} />
